38dc21cd56bbd18bfaf272cf96cb6b8b3122aca6
Seth <48872998+sethfl@users.noreply.github.com>
2020-02-24 18:54:58 -0500

    Update fedora-install.sh

--- scripts/fedora-install.sh
+++ scripts/fedora-install.sh
@@ -15,25 +15,19 @@ killall gnome-software
 rm -rf ~/.cache/gnome-software
 echo "gnome software reloaded"
 
-sudo dnf install htop neofetch transmission gnome-tweaks -y
+sudo dnf install neofetch gnome-tweaks -y
 flatpak update
 flatpak install flathub com.spotify.client
-flatpak install com.discordapp.Discord
+flatpak install flathub com.discordapp.Discord
 echo "apps installed"
-
-sudo dnf remove Rhythmbox gnome-maps -y
-echo "extra packages deleted"
 
 sudo dnf copr enable kwizart/fedy
 sudo dnf install fedy -y
 echo "fedy installed"
 
-sudo dnf install plymouth-theme-charge
+sudo dnf install plymouth-theme-charge -y
 sudo plymouth-set-default-theme charge -R
 echo "plymouth themed changed to charge. initrd rebuilt."
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-echo "oh my bash installed"
 
 sudo dnf update -y
 sudo dnf upgrade -y
