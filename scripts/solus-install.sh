9edb20bed7517cc7601d52e8802a4e9fc5d9605f
Seth <48872998+sethfl@users.noreply.github.com>
2020-03-15 19:27:56 -0400

    Update solus-install.sh

--- scripts/solus-install.sh
+++ scripts/solus-install.sh
@@ -57,18 +57,16 @@ echo "--------------------- done ---------------------"
 ## add dotfiles
 
 echo "--------------------- adding dotfiles ---------------------"
-mkdir ~/tmp
-git clone https://github.com/sethfl/dotfiles.git ~/tmp/
-cd ~/tmp/
-cp -r home/. ~
-cd ~
-rm -rf tmp
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+mkdir ~/tmp
+git clone https://github.com/sethfl/dotfiles.git ~/tmp
+cd ~/tmp
+cp -r home/. ~/
+cd ~
+rm -rf tmp
 clear
 echo "--------------------- done ---------------------"
-
 
 ## final system upgrade and cleanup
 
