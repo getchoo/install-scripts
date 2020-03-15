ccdb8352511ed9aef25f651cd3b8c42b29ebfeac
Seth <48872998+sethfl@users.noreply.github.com>
2020-03-15 19:35:05 -0400

    Create arch-post.sh

---
+++ scripts/arch-post.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+
+
+## install gnome and stuff lol
+
+echo ""--------------------- installing everything ---------------------"
+sudo pacman -S gnome vim 
+## set new shell for user
+
+
+## set new shell 
+
+echo "--------------------- setting new shell for user ---------------------"
+sudo pacman -S zsh git
+chsh -s /bin/zsh
+#clear
+echo "--------------------- done ---------------------"
+
+
+## add dotfiles
+
+echo "--------------------- adding dotfiles ---------------------"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+mkdir ~/tmp
+git clone https://github.com/sethfl/dotfiles.git ~/tmp
+cd ~/tmp
+cp -r home/. ~/
+cd ~
+rm -rf tmp
+clear
+
+
+
+## finish up
+echo "--------------------- done ---------------------" && exit 0
