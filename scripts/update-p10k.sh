2c43091a3f65b8bb47b85a4b7efbf272580da459
Seth Flynn <sethaflynn@gmail.com>
2020-09-29 22:06:53 -0400

    add an update script for p10k

---
+++ scripts/update-p10k.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export $ZDOTDIR=$HOME/.config/zsh
+
+rm -rf $ZDOTDIR/themes/powerlevel10k
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZDOTDIR/themes/powerlevel10k
+
+cd ~/dev/git/powerlevel10k 
+
+git checkout $(curl -s https://api.github.com/repos/romkatv/powerlevel10k/releases/latest \
+| grep "tag_name" \
+| sed 's/\"\|\"\|:\|tag_name\|,\|\ //g')
